cca9d1ccd0244fa67c951b7b314909972df2799e
innovator1311 <tri.m.nguyen.1999@gmail.com>
2021-12-12 22:34:53 +0700

    latest

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def train(**kwargs):
     writer = SummaryWriter()
     print('load data')
     dataloader = data_.DataLoader(dataset, \
-                                  batch_size=1, \
+                                  batch_size=8, \
                                   shuffle=True, \
                                   # pin_memory=True,
                                   num_workers=opt.num_workers
